b38d869377ebb81961c6f0809e6522637bda1798
lcy <43211937+lcy0950@users.noreply.github.com>
2021-04-24 20:47:59 +0800

    Update EventLoopThreadPool.cpp

--- WebServer/EventLoopThreadPool.cpp
+++ WebServer/EventLoopThreadPool.cpp
@@ -16,17 +16,17 @@ void EventLoopThreadPool::start() {
   for (int i = 0; i < numThreads_; ++i) {
     std::shared_ptr<EventLoopThread> t(new EventLoopThread());
     threads_.push_back(t);
-    loops_.push_back(t->startLoop());
+    loops_.push_back(t->startLoop());//startLoop会生成一个新的EventLoop并且进行Loop,最终不使用的时候会阻塞在eventLoop的Loop上.
   }
 }
 
 EventLoop *EventLoopThreadPool::getNextLoop() {
-  baseLoop_->assertInLoopThread();
-  assert(started_);
+  baseLoop_->assertInLoopThread();//这个函数只能够在主loop中使用
+  assert(started_);//表示此线程池一定已经开始工作了。
   EventLoop *loop = baseLoop_;
-  if (!loops_.empty()) {
+  if (!loops_.empty()) {//分配个几个Loop线程中的一个进行处理
     loop = loops_[next_];
     next_ = (next_ + 1) % numThreads_;
   }
-  return loop;
+  return loop;//返回被分配的那个线程,通过这种方式,将应对客户访问的压力进行分摊
 }
