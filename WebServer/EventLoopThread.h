5b1e5203adf5fbc289f265292aa9d1865aec665a
lcy <43211937+lcy0950@users.noreply.github.com>
2021-04-24 20:31:25 +0800

    Update EventLoopThread.h

--- WebServer/EventLoopThread.h
+++ WebServer/EventLoopThread.h
@@ -12,13 +12,13 @@ class EventLoopThread : noncopyable {
  public:
   EventLoopThread();
   ~EventLoopThread();
-  EventLoop* startLoop();
+  EventLoop* startLoop();//开始轮询
 
  private:
-  void threadFunc();
-  EventLoop* loop_;
-  bool exiting_;
-  Thread thread_;
-  MutexLock mutex_;
+  void threadFunc();//一个函数,他是用来干什么的？
+  EventLoop* loop_;//一个EventLoop,推测是从startLoop处来的,难道有多个线程在Loop?主线程只做accept，其余线程分摊处理连接嘛?
+  bool exiting_;//这个是用来干甚么的?
+  Thread thread_;//这个是用来干啥的?
+  MutexLock mutex_;//所和条件变量
   Condition cond_;
 };
