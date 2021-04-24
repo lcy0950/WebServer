33465167b4adbb4e81f33fa46f6cd34ae66c0974
lcy <43211937+lcy0950@users.noreply.github.com>
2021-04-24 20:27:47 +0800

    Update EventLoopThread.cpp

--- WebServer/EventLoopThread.cpp
+++ WebServer/EventLoopThread.cpp
@@ -4,14 +4,14 @@
 #include <functional>
 
 EventLoopThread::EventLoopThread()
-    : loop_(NULL),
+    : loop_(NULL),//loop为NULL，loop何时不为null,从哪生成?
       exiting_(false),
-      thread_(bind(&EventLoopThread::threadFunc, this), "EventLoopThread"),
-      mutex_(),
+      thread_(bind(&EventLoopThread::threadFunc, this), "EventLoopThread"),//绑定函数
+      mutex_(),//初始化条件变量
       cond_(mutex_) {}
 
 EventLoopThread::~EventLoopThread() {
-  exiting_ = true;
+  exiting_ = true;//为什么这里的exiting赋值为true,表示已经使用过了嘛?
   if (loop_ != NULL) {
     loop_->quit();
     thread_.join();
@@ -20,17 +20,17 @@ EventLoopThread::~EventLoopThread() {
 
 EventLoop* EventLoopThread::startLoop() {
   assert(!thread_.started());
-  thread_.start();
+  thread_.start();//猜想thread会设置一个loop
   {
-    MutexLockGuard lock(mutex_);
-    // 一直等到threadFun在Thread里真正跑起来
-    while (loop_ == NULL) cond_.wait();
+    MutexLockGuard lock(mutex_);//这里加锁
+    // 一直等到threadFun在Thread里真正跑起来,这里需要thread真正的start并且生成一个EventLoop之后
+    while (loop_ == NULL) cond_.wait();//等待唤醒,唤醒的条件是loop被设置了
   }
-  return loop_;
+  return loop_;//返回loop
 }
 
 void EventLoopThread::threadFunc() {
-  EventLoop loop;
+  EventLoop loop;//生成一个EventLoop
 
   {
     MutexLockGuard lock(mutex_);
