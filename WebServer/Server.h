883188df438a1338069fa75a7f4ce9b8a48e5f8b
lcy <43211937+lcy0950@users.noreply.github.com>
2021-04-24 16:52:54 +0800

    Update Server.h

--- WebServer/Server.h
+++ WebServer/Server.h
@@ -13,7 +13,7 @@ class Server {
   EventLoop *getLoop() const { return loop_; }//得到EventLoop对象
   void start();//开始
   void handNewConn();//处理新的连接. question:新连接在哪里存放?
-  void handThisConn() { loop_->updatePoller(acceptChannel_); }//这个函数起什么作用?
+  void handThisConn() { loop_->updatePoller(acceptChannel_); }//更新acceptChannel_的状态
 
  private:
   EventLoop *loop_;
