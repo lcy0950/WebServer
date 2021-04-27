beea30f61a8a80567aeedbfe255507be1c4a46ed
lcy <43211937+lcy0950@users.noreply.github.com>
2021-04-27 16:25:37 +0800

    Update Logging.cpp

--- WebServer/base/Logging.cpp
+++ WebServer/base/Logging.cpp
@@ -9,6 +9,12 @@
 #include <time.h>  
 #include <sys/time.h> 
 
+//本文件总结：
+//总结一下LOG的使用方法
+//LOG首先会建立一个Logger对象,对象的构造函数和<<符号重载使得能够将需要记录的内容放入LogStream中
+//然后此对象析构的时候,会将LogStream中储存的内容通过Asyn...Logger进行Append
+
+//下一步就是看看Asyn..是怎样start和append的
 
 static pthread_once_t once_control_ = PTHREAD_ONCE_INIT;
 static AsyncLogging *AsyncLogger_;
@@ -17,14 +23,14 @@ std::string Logger::logFileName_ = "./WebServer.log";
 
 void once_init()
 {
-    AsyncLogger_ = new AsyncLogging(Logger::getLogFileName());
-    AsyncLogger_->start(); 
+    AsyncLogger_ = new AsyncLogging(Logger::getLogFileName());//得到日志文件地址
+    AsyncLogger_->start(); //开始工作
 }
 
 void output(const char* msg, int len)
 {
-    pthread_once(&once_control_, once_init);
-    AsyncLogger_->append(msg, len);
+    pthread_once(&once_control_, once_init);//一次初始化
+    AsyncLogger_->append(msg, len);//向AsyncLogger里面写内容
 }
 
 Logger::Impl::Impl(const char *fileName, int line)
@@ -44,16 +50,16 @@ void Logger::Impl::formatTime()
     time = tv.tv_sec;
     struct tm* p_time = localtime(&time);   
     strftime(str_t, 26, "%Y-%m-%d %H:%M:%S\n", p_time);
-    stream_ << str_t;
+    stream_ << str_t;//标明发生时间
 }
 
 Logger::Logger(const char *fileName, int line)
   : impl_(fileName, line)
 { }
 
-Logger::~Logger()
+Logger::~Logger()//这里的析构函数 将LogStream 和 AsyncLogging联系到一起
 {
-    impl_.stream_ << " -- " << impl_.basename_ << ':' << impl_.line_ << '\n';
-    const LogStream::Buffer& buf(stream().buffer());
-    output(buf.data(), buf.length());
+    impl_.stream_ << " -- " << impl_.basename_ << ':' << impl_.line_ << '\n';//赋值构造函数
+    const LogStream::Buffer& buf(stream().buffer());//Buffer类型的
+    output(buf.data(), buf.length());//向异步Log组件写内容.
 }
